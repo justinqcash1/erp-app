1c5402977b97d8c2ec4a40f3b2d35f42977fa7bd
justinqcash1 <justin.q.cash@gmail.com>
2025-04-16 18:28:54 -0500

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>ERP Application</h1>
+      <p>Welcome to your ERP application.</p>
+    </main>
+  );
+}
